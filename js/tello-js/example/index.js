86811eac82a0abb9c3934cdc756a81a145d72e1b
Juanfran <juanfran.ag@gmail.com>
2019-10-15 17:06:09 +0200

    fly with promp?

--- js/tello-js/example/index.js
+++ js/tello-js/example/index.js
@@ -1,12 +1,26 @@
+setTimeout(() => {
+  console.log('end');
+  // process.exit();
+}, 10000);
+
 const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+const Writable = require('stream').Writable;
+
+var mutableStdout = new Writable({
+  write: (chunk, encoding, callback) => {
+    if (!this.muted) {
+      process.stdout.write(chunk, encoding);
+    }
+
+    callback();
+  }
 });
 
 const dgram = require('dgram');
 
 const CONFIG = require('./config.json');
+
+let SPEED = 100;
 
 // Connect Drone
 const drone = dgram.createSocket('udp4');
@@ -17,28 +31,14 @@ droneState.bind(CONFIG.STATE_PORT);
 
 // keypress events
 readline.emitKeypressEvents(process.stdin);
-process.stdin.setRawMode(false);
+process.stdin.setRawMode(true);
 
-rl.question(`Input?`, (name) => {
-  console.log(`-------------> ${name}!`);
-  process.stdin.setRawMode(true);
+function runCommand(command) {
+  console.log('command', command);
+}
 
-  const onKeypress = (str, key) => {
-    if (key.ctrl && key.name === 'c') {
-      process.exit();
-    } else {
-      console.log(`You pressed the "${str}" key`);
-      console.log(key);
-      console.log();
-    }
-  };
-
-  process.stdin.on('keypress', onKeypress);
-
-  setTimeout(() => {
-    process.stdin.off('keypress', onKeypress);
-  },3000);
-});
+console.log('Press any key to take off');
+console.log();
 
 droneState.on('message', (message) => {
   let state = message
@@ -59,3 +59,54 @@ droneState.on('message', (message) => {
 drone.on('message', (message) => {
   console.log(`drone : ${message}`);
 });
+
+let takeoff = false;
+
+const onKeypress = (str, key) => {
+  if (!takeoff) {
+    runCommand('command');
+    runCommand(`speed ${SPEED}`);
+    runCommand('takeoff');
+
+    takeoff = true;
+  } else {
+    if (key.ctrl && key.name === 's') {
+      process.stdin.setRawMode(false);
+      process.stdin.off('keypress', onKeypress);
+
+      const rl = readline.createInterface({
+        input: process.stdin,
+        output: mutableStdout
+      });
+
+      mutableStdout.stdoutMuted = true;
+
+      rl.question(`Set new speed: `, (newSpeed) => {
+        SPEED = newSpeed;
+        runCommand(`speed ${SPEED}`);
+
+        process.stdin.setRawMode(true);
+        process.stdin.on('keypress', onKeypress);
+
+        rl.close();
+      });
+    } else if (key.ctrl && key.name === 'c') {
+      runCommand('land');
+      process.exit();
+    } else {
+      console.log(`You pressed the "${str}" key`);
+      console.log(key);
+      console.log();
+    }
+  }
+};
+
+const onKeyup = (str, key) => {
+  console.log(`You pressed the "${str}" key`);
+  console.log(key);
+  console.log();
+};
+
+process.stdin.on('keypress', onKeypress);
+
+
